38dc9d09cf42a99ff8671fb61b0f2af1a1177abd
maitreebain <maitreebain@gmail.com>
2020-01-13 17:41:25 -0500

    added all signs

--- Pursuit-Core-iOS-UserDefaults-Lab/UserPreferences.swift
+++ Pursuit-Core-iOS-UserDefaults-Lab/UserPreferences.swift
@@ -75,9 +75,57 @@ class UserPreference {
                 return Zodiac.taurus
             }
         case .may(5):
-            if day 
+            if day <= 21 && day > 0 {
+                return Zodiac.taurus
+            } else if day <= 31 {
+                return Zodiac.gemini
+            }
+        case .june(6):
+            if day <= 21 && day > 0 {
+                return Zodiac.gemini
+            } else if day <= 30 {
+                return Zodiac.cancer
+            }
+        case .july(7):
+            if day <= 22 && day > 0{
+                return Zodiac.cancer
+            } else if day <= 30{
+                return Zodiac.leo
+            }
+        case .august(8):
+            if day <= 22 && day > 0 {
+                return Zodiac.leo
+            } else if day < 31 {
+                return Zodiac.virgo
+            }
+        case .september(9):
+            if day <= 22 && day > 0 {
+                return Zodiac.virgo
+            } else if day <= 30 {
+                return Zodiac.libra
+            }
+        case .october(10):
+            if day <= 22 && day > 0 {
+                return Zodiac.libra
+            } else if day <= 31 {
+                return Zodiac.scorpio
+            }
+        case .november(11):
+            if day <= 21 && day > 0 {
+                return Zodiac.scorpio
+            } else if day <= 30 {
+                return Zodiac.sagittarius
+            }
+        case .december(12):
+            if day <= 22 && day > 0 {
+                return Zodiac.sagittarius
+            } else if day <= 31 {
+                return Zodiac.capricorn
+            }
+        @unknown default:
+            print("default")
         }
-        
+        return Zodiac.gemini
     }
     
 }
